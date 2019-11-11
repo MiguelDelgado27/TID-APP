a4b362b305cdfdc54d9a41b35a34d7698f2d88f4
Giancarlo Tupac Gutierrez <giancarlo.tupac@gmail.com>
2019-11-11 00:01:45 -0500

    update_num_questions

--- src/constants/numOfQuestions.js
+++ src/constants/numOfQuestions.js
@@ -1,6 +1,6 @@
 const NUM_OF_QUESTIONS = [];
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 80; i++) {
   const object = { key: i, text: `${i}`, value: i };
   NUM_OF_QUESTIONS.push(object);
 }
